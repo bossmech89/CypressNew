7b548b5c935a9a864e52809c4ee35e19ad8fa23d
Baskaran <bossmech89@gmail.com>
2022-10-10 22:27:22 +0530

    dynamic file

---
+++ cypress/e2e/dynamicProperties.cy.js
@@ -0,0 +1,30 @@
+// Wait for elements will be enabled
+function waitFiveSeconds(millisec) {
+    return new Promise(resolve => {
+        setTimeout(() => { resolve('') }, millisec)
+    })
+};
+
+describe('Validate the buttons', () => {
+    it('passes', () => {
+        cy.visit('https://demoqa.com/upload-download');
+    });
+
+    it('Click on Dynamic Properties menu', () => {
+        cy.contains('Dynamic Properties').click();
+    });
+
+    it('Validate the dynamic properties', async () => {
+        cy.get('#enableAfter').should('be.disabled');
+        // async function myFunction(){
+        await waitFiveSeconds(8000);
+        // };
+        // myFunction();
+    });
+
+    it('Click on "Will enable 5 seconds" button', () => {
+        // cy.wait(5000);
+        cy.get('#enableAfter').click({force: true});
+        cy.get('#enableAfter').should('be.enabled');
+    });
+});
